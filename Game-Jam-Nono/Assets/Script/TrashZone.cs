71afb43315b06f3c587d55c0197c318159940ebb
Emrys <legall.emrys@gmail.com>
2024-12-12 14:32:10 +0100

    attention j'arrive

--- Game-Jam-Nono/Assets/Script/TrashZone.cs
+++ Game-Jam-Nono/Assets/Script/TrashZone.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TrashZone : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI eboueurTeamScore;
+    [SerializeField] TextMeshProUGUI raccoonTeamScore;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +24,21 @@ public class TrashZone : MonoBehaviour
 
         if (collision.gameObject.tag == gameObject.tag && collision.gameObject.GetComponent<Trash>())
         {
-            
+            Debug.Log("Trash get");
+            collision.gameObject.SetActive(false);
         }
         else
         {
 
         }
+    }
+
+    void UpdateTeamScore(TextMeshProUGUI teamScore)
+    {
+        teamScore.text += 1;
+    }
+    void UpdatePlayerScore(TextMeshProUGUI playerScore, int amount)
+    {
+        playerScore.text += amount;
     }
 }
