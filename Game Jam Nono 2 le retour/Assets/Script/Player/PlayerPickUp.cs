a0113e92e3fcf4593e865dde8271413e1983f0c8
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2024-12-16 10:26:59 +0100

    Les connnnntrwolll

---
+++ Game Jam Nono 2 le retour/Assets/Script/Player/PlayerPickUp.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
+
+public class PlayerPickUp : NetworkBehaviour
+{
+    [SerializeField] float raycastDistance;
+    [SerializeField] LayerMask pickupLayer;
+    [SerializeField] Transform pickupPosition;
+    [SerializeField] GameObject body;
+
+    Camera cam;
+    bool hasObjectInHand;
+    GameObject objInHand;
+    Transform objectHolder;
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (!base.IsOwner)
+            enabled = false;
+
+        cam = Camera.main;
+        
+    }
+
+    public void OnGrab(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.started) { return; }
+        
+        Grab();
+    }
+
+    void Grab()
+    {
+        // Draw the raycast in the scene view for debugging
+        Debug.DrawRay(body.transform.position, body.transform.forward * raycastDistance, Color.green, 0.1f);
+
+        if (Physics.Raycast(body.transform.position, body.transform.forward, out RaycastHit hit, raycastDistance, pickupLayer))
+        {
+            if (!hasObjectInHand)
+            {
+                SetObjectInHandServer(hit.transform.gameObject, pickupPosition.position, pickupPosition.rotation, gameObject);
+                objInHand = hit.transform.gameObject;
+                hasObjectInHand = true;
+            }
+            else if (hasObjectInHand)
+            {
+                Drop();
+
+                SetObjectInHandServer(hit.transform.gameObject, pickupPosition.position, pickupPosition.rotation, gameObject);
+                objInHand = hit.transform.gameObject;
+                hasObjectInHand = true;
+            }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void SetObjectInHandServer(GameObject obj, Vector3 position, Quaternion rotation, GameObject player)
+    {
+        SetObjectInHandObserver(obj, position, rotation, player);
+    }
+
+    [ObserversRpc]
+    void SetObjectInHandObserver(GameObject obj, Vector3 position, Quaternion rotation, GameObject player)
+    {
+        obj.transform.position = position;
+        obj.transform.rotation = rotation;
+        obj.transform.parent = player.transform;
+
+        if (obj.GetComponent<Rigidbody>() != null)
+            obj.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void Drop(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.started) { return; }
+
+        Drop();
+    }
+
+    void Drop()
+    {
+        if (!hasObjectInHand)
+            return;
+
+        DropObjectServer(objInHand, objectHolder);
+        hasObjectInHand = false;
+        objInHand = null;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void DropObjectServer(GameObject obj, Transform worldHolder)
+    {
+        DropObjectObserver(obj, worldHolder);
+    }
+
+    [ObserversRpc]
+    void DropObjectObserver(GameObject obj, Transform worldHolder)
+    {
+        obj.transform.parent = worldHolder;
+
+        if (obj.GetComponent<Rigidbody>() != null)
+            obj.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
